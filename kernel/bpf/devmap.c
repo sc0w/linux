e7ac9b8743663cacea86fe5dc076a6221014236e
ZenWalker <scow@riseup.net>
2021-06-02 11:41:25 +0200

    bmp, devmap: avoid redundant condition

--- kernel/bpf/devmap.c
+++ kernel/bpf/devmap.c
@@ -133,9 +133,7 @@ static int dev_map_init_map(struct bpf_dtab *dtab, union bpf_attr *attr)
 
 		if (!dtab->n_buckets) /* Overflow check */
 			return -EINVAL;
-	}
 
-	if (attr->map_type == BPF_MAP_TYPE_DEVMAP_HASH) {
 		dtab->dev_index_head = dev_map_create_hash(dtab->n_buckets,
 							   dtab->map.numa_node);
 		if (!dtab->dev_index_head)
